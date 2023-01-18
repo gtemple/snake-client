535a545644a786ca4d9dd766eb1aaba65ce1d4e1
Giordano Temple <giordanotemple@gmail.com>
2023-01-18 20:53:36 +0000

    added name

--- client.js
+++ client.js
@@ -1,4 +1,6 @@
-const connect = function () {
+const net = require("net");
+
+const connect = () => {
   const conn = net.createConnection({
     host: '165.227.47.243',
     port: '50541',
@@ -8,8 +10,18 @@ const connect = function () {
     console.log(data)
   });
 
+  conn.on("connect", () =>{
+    console.log("Time to be a snake.")
+  })
+
+  conn.on("connect", () => {
+    conn.write("Name: BRO" );
+  });
+
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
   return conn;
 };
+
+module.exports = connect;
